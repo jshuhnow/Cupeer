4edc2a9ad7a292fbdcd32c1ac906d89d14deaabf
Jason Huh <jaeseok.huh.0@gmail.com>
2019-12-02 16:59:43 +0900

    Implement a permission handling (#1)

--- app/src/main/java/com/example/cupid/view/MainActivity.kt
+++ app/src/main/java/com/example/cupid/view/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.cupid.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cupid.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.cupid.model.ModelModule
 import com.example.cupid.model.observer.DomainObserver
 
 class MainActivity : AppCompatActivity(), DomainObserver {
     private val model = ModelModule.dataAccessLayer
+    private val MULTIPLE_PERMISSIONS = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.cupid.R.layout.activity_main)
+
+        checkPermissions()
     }
     override fun onStart() {
         super.onStart()
@@ -23,4 +29,76 @@ class MainActivity : AppCompatActivity(), DomainObserver {
         model.unregister(this)
     }
 
+
+    private fun checkPermissions() : Boolean {
+        /* https://developer.android.com/training/permissions/requesting */
+
+        // Here, thisActivity is the current activity
+        val arrPermissions = arrayOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        val arrNeededPermissions = ArrayList<String>()
+
+        for (permission in arrPermissions) {
+            if (ContextCompat.checkSelfPermission(this,
+                    permission)
+            != PackageManager.PERMISSION_GRANTED) {
+                arrNeededPermissions.add(permission)
+            }
+        }
+
+        return if (arrNeededPermissions.isNotEmpty()) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(
+                this,
+                arrNeededPermissions.toArray() as Array<out String>,
+                MULTIPLE_PERMISSIONS
+            )
+            false
+        } else {
+            // Permission has already been granted
+            true
+        }
+
+        /* https://developer.android.com/training/permissions/requesting END */
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MULTIPLE_PERMISSIONS -> {
+
+                if (grantResults.isNotEmpty()) {
+                    var permissionsDenied = ""
+                    for (permission in permissions) {
+                        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                            permissionsDenied += "\n" + permission
+                        }
+
+                    }
+
+
+                } else {
+                    // One of the permission has not been granted
+                }
+
+                // TODO: update view
+
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
 }
