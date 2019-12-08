2c416bd7d5fb9112885774cdedc4131e595718f7
Jason Huh <jaeseok.huh.0@gmail.com>
2019-12-08 14:25:44 +0900

    Remove unused part

--- app/src/main/java/com/example/cupid/controller/ControllerModule.kt
+++ app/src/main/java/com/example/cupid/controller/ControllerModule.kt
@@ -3,6 +3,5 @@ package com.example.cupid.controller
 import com.example.cupid.model.ModelModule
 
 object ControllerModule {
-    fun loginContrroller() = LoginController(ModelModule.dataAccessLayer)
-    //fun nearbyController() = NearbyController(ModelModule.dataAccessLayer)
+    
 }
