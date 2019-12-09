cb0da2a400125381981a7e35907e43f852464241
Jaeseok Huh (Jason) <jaeseok.huh.0@gmail.com>
2019-12-09 21:13:20 +0900

    Parcelize

--- app/src/main/java/com/example/cupid/model/domain/Answer.kt
+++ app/src/main/java/com/example/cupid/model/domain/Answer.kt
@@ -1,6 +1,10 @@
 package com.example.cupid.model.domain
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Answer (
     val questionId : Int = -1,
     val answerId : Int = -1
-)
+) : Parcelable
